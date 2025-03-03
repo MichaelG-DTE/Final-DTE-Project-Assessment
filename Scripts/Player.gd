01193a3b22160f42c1f692181066060c7b6eb873
MichaelG-DTE <21051@rosmini.school.nz>
2025-03-04 09:59:26 +1300

    Added Background, modified script for player. Movement is now working

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,3 +1,15 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@export var speed = 300
+@onready var screensize = get_viewport_rect().size
+
+func _process(delta):
+	var direction = Input.get_vector("Left", "Right", "Up", "Down")
+	if direction.x > 0:
+		$ShipTexture.animation = "Right_Flying"
+	elif direction.x < 0:
+		$ShipTexture.animation = "Left_Flying"
+	else: 
+		$ShipTexture.animation = "Flying"
+	velocity = direction * speed
+	move_and_slide()
