3361f60f16e326f3774e1162af26f021d4bd9cee
MichaelG-DTE <21051@rosmini.school.nz>
2025-03-05 15:07:29 +1300

    Added Thrusters (Need Reworking) + Added Player Spawn Point  + Game Script (Central Process for the whole game)

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -7,9 +7,14 @@ func _process(delta):
 	var direction = Input.get_vector("Left", "Right", "Up", "Down")
 	if direction.x > 0:
 		$ShipTexture.animation = "Right_Flying"
+		$ShipTexture/Thrusters.animation = "Right_Thrusters"
+	
 	elif direction.x < 0:
 		$ShipTexture.animation = "Left_Flying"
+		$ShipTexture/Thrusters.animation = "Left_Thrusters"
+		
 	else: 
 		$ShipTexture.animation = "Flying"
+		$ShipTexture/Thrusters.animation = "Thrusters"
 	velocity = direction * speed
 	move_and_slide()
